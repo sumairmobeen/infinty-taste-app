9c0c03fef494b203f7e86d7849b1d74f3bce4cce
sultanbaloch <gsarwar162@gmail.com>
2021-03-22 21:44:38 +0500

    changes

--- server.js
+++ server.js
@@ -32,7 +32,20 @@ var upload = multer({ storage: storage })
 
 
 
-var SERVICE_ACCOUNT = JSON.parse(SERVICE_ACCOUNT);
+var SERVICE_ACCOUNT =
+{
+    "type": "service_account",
+    "project_id": "infinity-taste",
+    "private_key_id": "7cb03068179121c1063d27513ac3b0c328dcea34",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCXI86EUhRJrvcr\nK19OjsUq/SjTtVui96Uj2b1e+e+qQtwJluiWPny6v2HdoWuHQKLixW5psmyYbr7k\nYur/RHR4ExB+5bURZycHpYmE/wbs5NRNrlyycq6sOnPySCFFTempXVf3FJs0ZQJ4\nBbOHvdvLbjMWSGYVdfyeDBRvdz4BtN5bjHHDsi/FoXuFGbB4rCTsa/XCPYvJ8zJp\nf87lAwBmkaI/7/Kip9rzBhHb/+i3VYxnNJenMXAhax2uUsSh0DYCATO/EJvTmvM0\nI8uxH2H9aAGfN3NoAqXP4JpCdkWWxOVQgFF2vnZZuWFIPiR94uFmGE4gg2/pqn86\nBT4u9E+PAgMBAAECggEAEcthcSrIwLQc3bRJkFI23IzZ1bMwistlYgcsyJXcpcsh\nZO+jiqzouoP13cY3hsERbGmaSk97gaboSrNqv4sru0djVc1tuBLRAZF303TDN/jp\nFgxFVa0y64wmQoxDfolhzVL0HF8Mtup9k+v36y9rRVRAJ4t2mWg+yczaSxVa85BN\nhJgPTWhWdRemkbqTfzGA2XGZwEpPr7tq1u5uz+5IvyBInwyoUPKNvh/yL3cveQAc\n/lOuWEQ5Jqcu27WJhjLquqBcMX0T+w6/4Yf5gQpcePwmsA+8Eca+74/nVxD9Ocl2\ngYpQJK8f/3XBWmCfW439P/fWwUzp0Jy3ijT9YlOR4QKBgQDRhWUpoa2kk0p84zPt\n1n21Ss4whpo4VgWj6wzBbQKoWXW9tGlmHYAr8pmiqvTlUeQuNr+CX18q4/AtPwFS\nyGACX+8bcv4LILr0uRauJ2pPD4/tbxTuJFLjAff6u4sdjiQowpomQ/AAoXtPIr8f\nCSPIJ4mB5C30hr2ZCLMYsBT5bQKBgQC4qvhOMFzNG0+S9TPA+3Z/MYbWji6sby4a\nBzuFfnfC+D02Lz+zv9nTV48Pyq1ats7+9FEYmZ53T0Cg+kDylomEzRlu5XHNm9NJ\n++ssXi64TxnIIy510dwQGTHIkahCnda9hC8apJB2CDWM5mSN4JiV7ZFclnSyzyI+\nezk1133rawKBgQC48iE8QIVAsuublgrutNk+QIwFdauIcxaDfSZDT3B7Hoy6Khin\nlP7gzEMlvfZyvnjG+njOIlWlTJNjvSvz4QAu2HP+LOFm/TtKbl9A13YV18zab6Vw\nGQw5ajF1WDsSmbf9atobUPj39ZMdZbSDNtQpCA1vZPXT1WRjQ7Sj5Dp/eQKBgAQ0\n+OUnGuYdSinJ5jzuPZvWMAExSgTpn34+Te/TmrzZVOOoHeZlZ3v2Ea9Fab+YTIzo\nWfSag06qgoloShR8s3NQDNVG6tnJQ/36fDgNiTjvrGGv034hosbHHCg6kT+7Qsoc\neco8l4Ho2/dyxDWrOS6pM9eNIel06I3N1tbQKvEzAoGACRPEXKf+vIVAR4qcFstk\nEJjgCUqVucSEmurqjhWXG+CdgOmOo5264mWJzkz1hIPPJijMfWFVRzMhT+JQZVTM\nHVD32ecUirM6u3RPfkJR2LQA9GfPp4CRposzS3YGJ8j37TDGRkA+4bBHoMdGxTM9\nOilPT+ymjidh9hTmfbC4qjQ=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-kv9sp@infinity-taste.iam.gserviceaccount.com",
+    "client_id": "118243715814975574793",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kv9sp%40infinity-taste.iam.gserviceaccount.com"
+}
+
 
 
 admin.initializeApp({
